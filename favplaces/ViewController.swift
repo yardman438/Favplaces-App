cd7067194f950db33ffe4ee49c2157f1caf48a48
Serhii Dvornyk <serhii.dvornyk@gmail.com>
2022-06-08 22:23:19 +0300

    Initial Commit

---
+++ favplaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  favplaces
+//
+//  Created by Serhii Dvornyk on 08.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
